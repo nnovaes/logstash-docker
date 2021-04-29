c2d70b8d110db6636e623c204b7c8156413d02d2
Norman Novaes <novaes@mailbox.org>
2021-04-29 09:28:23 -0400

    update

---
+++ build.sh
@@ -0,0 +1,9 @@
+docker build -t normannovaes/logstash-docker:7.12.1_pluginbase -f pluginBase.Dockerfile .
+docker build -t normannovaes/logstash-docker:7.12.1_oss_slim -f oss_slim.Dockerfile .
+docker build -t normannovaes/logstash-docker:7.12.1_oss_plugins -f oss_slim.Dockerfile .
+docker build -t normannovaes/logstash-docker:7.12.1_oss_full -f oss_full.Dockerfile .
+docker push normannovaes/logstash-docker:7.12.1_pluginbase
+docker push normannovaes/logstash-docker:7.12.1_oss_slim
+docker push normannovaes/logstash-docker:7.12.1_oss_plugins
+docker push normannovaes/logstash-docker:7.12.1_oss_full
+
