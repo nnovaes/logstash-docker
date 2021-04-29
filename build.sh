a7e6a0fcd1a5c262174309d1bd7f7b741b0324bc
Norman Novaes <novaes@mailbox.org>
2021-04-29 09:47:23 -0400

    corrected build.sh pointing to proper dockerfile; added list of available plugins in plugin image

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t normannovaes/logstash-docker:7.12.1_pluginbase -f pluginBase.Dockerfile .
 docker build -t normannovaes/logstash-docker:7.12.1_oss_slim -f oss_slim.Dockerfile .
-docker build -t normannovaes/logstash-docker:7.12.1_oss_plugins -f oss_slim.Dockerfile .
+docker build -t normannovaes/logstash-docker:7.12.1_oss_plugins -f oss_plugins.Dockerfile .
 docker build -t normannovaes/logstash-docker:7.12.1_oss_full -f oss_full.Dockerfile .
 docker push normannovaes/logstash-docker:7.12.1_pluginbase
 docker push normannovaes/logstash-docker:7.12.1_oss_slim
--- oss_plugins.Dockerfile
+++ oss_plugins.Dockerfile
@@ -5,7 +5,8 @@ FROM normannovaes/logstash-docker:7.12.1_oss_slim
  # install plugins
 COPY --from=0 /usr/share/logstash/oss_pluginpack.zip .
 RUN /usr/share/logstash/bin/logstash-plugin install file:///oss_pluginpack.zip && \
-    /usr/share/logstash/bin/logstash-plugin update
+    /usr/share/logstash/bin/logstash-plugin update && \
+    /usr/share/logstash/bin/logstash-plugin list > /usr/share/logstash/config/pluginList.txt 
 
 # copy configs
 COPY ./config/ /usr/share/logstash/config
