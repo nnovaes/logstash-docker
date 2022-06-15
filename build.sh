b6bdb5e74e1865524c00d8f6c378ff5a99057ed4
Norman Novaes <novaes@mailbox.org>
2022-06-15 17:21:13 -0400

    update to 8x bases

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ docker push normannovaes/logstash-docker:8.2.3_oss_slim
 docker build -t normannovaes/logstash-docker:8x_pluginBase -f pluginBase.Dockerfile .
 docker push normannovaes/logstash-docker:8x_pluginBase
 
-docker build -t normannovaes/logstash:oss8x_slim_x86_64 -f oss8x_plugins.Dockerfile .
-docker push normannovaes/logstash:oss8x_slim_x86_64
+docker build -t normannovaes/logstash:oss8x_plugins_x86_64 -f oss8x_plugins.Dockerfile .
+docker push normannovaes/logstash:oss8x_plugins_x86_64
 
 docker build -t normannovaes/logstash-docker:8.2.3_oss_full -f oss_full.Dockerfile .
 docker push normannovaes/logstash-docker:8.2.3_oss_full
